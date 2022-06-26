2716d58e4a936165305738798e17960bc833244b
Claudio Martins <claudio.martins@taxpricingit.com.br>
2022-06-26 11:13:16 -0300

    verificando se a tabela settings existe

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class SettingServiceProvider extends ServiceProvider
@@ -28,6 +31,12 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // only use the Settings package if the Settings table is present in the database
+        if (!App::runningInConsole() && count(Schema::getColumnListing('settings'))) {
+            $settings = Setting::all();
+            foreach ($settings as $key => $setting) {
+                Config::set('settings.' . $setting->key, $setting->value);
+            }
+        }
     }
 }
