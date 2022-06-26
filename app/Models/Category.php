fe99a643cf7c37019286e0b070c5069eb4cceca1
Claudio Martins <claudio.martins@taxpricingit.com.br>
2022-06-26 12:23:25 -0300

    Gerenciamento de categorias

---
+++ app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'parent_id', 'featured', 'menu', 'image'
+    ];
+
+    protected $casts = [
+        'parent_id' =>  'integer',
+        'featured'  =>  'boolean',
+        'menu'      =>  'boolean'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::of($value)->slug('-');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+}
